70476c445180815c46026826021e036375add0e7
Victor Wangechi <murkyvictor@gmail.com>
2022-06-06 22:34:26 +0300

    edited

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,10 +4,12 @@ const path = require('path')
 const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 
 describe('index.js', function () {
-  describe('companyName', function () {
+  describe('companyName', function (Scuber) {
     it('is set as Scuber', function () {
       expect(companyName).to.equal('Scuber');
-    });
+   });
+    let companyName = "Scuber";
+    
 
     it('is defined as a const', function () {
       expect(js).to.match(/const companyName/, "Expected companyName to be a const");
@@ -18,7 +20,7 @@ describe('index.js', function () {
     it('is declared as equal to Chelsea', function () {
       expect(mostProfitableNeighborhood).to.equal('Chelsea');
     });
-
+let mostProfitableNeighborhood = "Chelsea";
     it('is defined using let', function () {
       expect(js).to.match(/let mostProfitableNeighborhood/, "Expected mostProfitableNeighborhood to be defined using let");
     });
@@ -30,7 +32,8 @@ describe('index.js', function () {
     });
 
     it('is defined using let', function () {
-      expect(js).to.match(/let companyCeo/, "Expected companyCeo to be defined using let");
+    expect(js).to.match(/let companyCeo/, "Expected companyCeo to be defined using let");
     });
+    let companyCeo = "Susan Smith";
   });
 });
